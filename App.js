092d3b074719a5b8c5e3683957536a3901213676
Khyati <khyati@neoscript.in>
2023-01-19 13:08:22 +0530

    Igniting our app

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
 const heading1=React.createElement('h1',{
     style:{
         color:"yellow"
@@ -7,8 +10,10 @@ const heading1=React.createElement('h1',{
 const heading2 = React.createElement('h2',{},"Header Two")
 
 const heading3= React.createElement('h3',{
-    class:"hello"
+    className:"hello"
 },"Header Three")
 
+const div=React.createElement('div',{},[heading1,heading2,heading3])
+
 const root=ReactDOM.createRoot(document.getElementById('root'));
-root.render([heading1,heading2,heading3])
+root.render(div)
